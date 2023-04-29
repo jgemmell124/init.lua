cbd4783398fb7024720feea74fb9cfd924faf712
john <johngemmell124@gmail.com>
2023-04-29 11:42:09 -0400

    tabstops

--- init.lua
+++ init.lua
@@ -1,6 +1,23 @@
 require("johnny.remap")
+require("johnny.packer")
 
 local opt=vim.opt
+
 opt.number=true
 opt.relativenumber=true
+
+opt.tabstop=4
+opt.shiftwidth=4
+opt.expandtab=true
+opt.smartindent=true
+
+opt.termguicolors=true
+
+opt.colorcolumn="80"
+
+-- Show partial commands
+opt.showcmd=true
+
+-- Show matching brackets
+opt.showmatch=true
 
