51636e0aca6eab9383bb652bb658094576f50344
john <johngemmell124@gmail.com>
2023-05-01 22:24:34 -0400

    indenting keymap

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,3 +1,25 @@
+
+-- REMAPS --
+
+-- Shortened function name 
+local keymap = vim.keymap.set
+
+-- Change the leader to a space
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "jk", "<ESC>")
+
+-- Go to previous item 
+keymap("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- Insert --
+-- Press jk for faster escape  
+keymap("i", "jk", "<ESC>")
+
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
+
+-- Prevent vim from replacing paste buffer
+keymap("v", "p", '"_dP')
