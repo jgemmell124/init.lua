6c2c23ed57aa1c9378d86ea2112e476f47432491
john <johngemmell124@gmail.com>
2023-05-01 22:47:33 -0400

    forgot closing comma

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -7,7 +7,7 @@ npairs.setup({
         lua = {'string'},-- it will not add a pair on that treesitter node
         javascript = {'template_string'},
         java = false,-- don't check treesitter on java
-    }
+    },
     disable_filetype = {"TelescopePrompt"},
 })
 
