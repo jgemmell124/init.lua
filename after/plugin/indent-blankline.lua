2b5f9859748908fb546cf8a44bada8015aec92a3
john <johngemmell124@gmail.com>
2023-05-15 15:13:28 -0400

    indent corlong

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -1,5 +1,15 @@
-vim.opt.list = true
+vim.cmd [[highlight IndentBlanklineIndent guifg=#5C5C5C gui=nocombine]]
+vim.cmd [[highlight IndentBlankLineSpace none]]
 
+vim.opt.list = true
 require("indent_blankline").setup {
     show_end_of_line = true,
+    show_current_context_start = false,
+    show_current_context = false,
+    show_current_context_start_on_current_line = false,
+    indent_blankline = false,
+    space_char_blankline = "",
+    char_highlight_list = {
+        "IndentBlanklineIndent",
+    }
 }
