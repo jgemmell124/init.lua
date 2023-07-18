66de8609372c26d2b5754e38bec75461a40dedb5
john <johngemmell124@gmail.com>
2023-07-18 18:36:46 -0400

    removed nvim tree from packer

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -65,15 +65,15 @@ return require('packer').startup(function(use)
 	use("nvim-treesitter/playground")
 
     -- Nvim Tree
-    use { "nvim-tree/nvim-tree.lua",
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
-            'kyazdani42/nvim-web-devicons'
-        },
-        config = function()
-            require("nvim-tree").setup {}
-        end
-    }
+    --[[ use { "nvim-tree/nvim-tree.lua", ]]
+    --[[     requires = { ]]
+    --[[         'nvim-tree/nvim-web-devicons', -- optional ]]
+    --[[         'kyazdani42/nvim-web-devicons' ]]
+    --[[     }, ]]
+    --[[     config = function() ]]
+    --[[         require("nvim-tree").setup {} ]]
+    --[[     end ]]
+    --[[ } ]]
 
     -- Harpoon
     use 'ThePrimeagen/harpoon'
