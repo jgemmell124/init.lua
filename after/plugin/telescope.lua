5a73d6099acbe00345642c66635db7f05b8e506e
john <johngemmell124@gmail.com>
2023-07-18 18:34:34 -0400

    typo

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,7 +4,7 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pr', builtin.resume, {})
-vim.keymap.set('n', '<leader>pb', builtin.buffer, {})
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
