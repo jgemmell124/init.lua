8d5a3a7689e982a82da813a424b5dcb42d8afeb4
john <johngemmell124@gmail.com>
2023-08-29 23:37:58 -0400

    Removed markdown

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -32,14 +32,6 @@ return require('packer').startup(function(use)
     }
 
 	use 'folke/todo-comments.nvim'
-	-- markdown preview
-   -- install without yarn or npm
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function() vim.fn["mkdp#util#install"]() end,
-	})
-
-	use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
 	use 'tpope/vim-fugitive'
 
