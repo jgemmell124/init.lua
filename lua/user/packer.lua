4d36fc69b64b6ae393c5855d13e7a188d0d3a678
john <johngemmell124@gmail.com>
2023-08-29 23:52:52 -0400

    Cleanup

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,13 +1,53 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
--- TODO 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+
+    -- Telescope for file searching 
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '1.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    -- LSP (Lsp Zero)
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
+
+    -- Tree Sitter
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use 'nvim-treesitter/playground'
+
     -- autopair brackets, quotes, etc
     use 'windwp/nvim-autopairs'
+
+	-- Show diagnostics
+	use 'folke/trouble.nvim'
+
+	-- Git plugin
+	use 'tpope/vim-fugitive'
+
     -- commenting lines
     use 'numToStr/Comment.nvim' -- Easily Comment stuff
     use 'JoosepAlviste/nvim-ts-context-commentstring' -- treessitter context
@@ -31,9 +71,8 @@ return require('packer').startup(function(use)
         requires = 'nvim-tree/nvim-web-devicons'
     }
 
+	-- todo comments that are highlighted
 	use 'folke/todo-comments.nvim'
-
-	use 'tpope/vim-fugitive'
 
     -- Lua Line 
     use({
@@ -41,54 +80,15 @@ return require('packer').startup(function(use)
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     })
 
-    -- Telescope
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '1.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-
-    -- Tree Sitter
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('HiPhish/nvim-ts-rainbow2')
-    use("nvim-treesitter/nvim-treesitter-context");
-	use("nvim-treesitter/playground")
-
     -- Harpoon
     use 'ThePrimeagen/harpoon'
 
-    -- LSP (Lsp Zero)
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            run = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-            },
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-        }
-    }
-
-	use 'folke/trouble.nvim'
-
     -- COLOR THEMES --
     -- Color theme (Gruv box)
-    use "ellisonleao/gruvbox.nvim"
+    use 'ellisonleao/gruvbox.nvim'
     use 'navarasu/onedark.nvim'
     use 'marko-cerovac/material.nvim'
 	-- the best vscode theme 
 	use 'lunarvim/darkplus.nvim'
 
 end)
-
-
