cbc205752c937834042b8496da5309b289cfd404
john <johngemmell124@gmail.com>
2023-09-15 20:44:16 -0400

    autopari

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -30,6 +30,8 @@ return require('packer').startup(function(use)
 
             -- Autocompletion
             {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
 			{'hrsh7th/cmp-nvim-lsp'}, -- Required
 			{'L3MON4D3/LuaSnip'},     -- Required
         }
@@ -41,6 +43,8 @@ return require('packer').startup(function(use)
 
     -- autopair brackets, quotes, etc
     use 'windwp/nvim-autopairs'
+	-- TODO check if autopairs can do this
+	use 'windwp/nvim-ts-autotag'
 
 	-- Show diagnostics
 	use 'folke/trouble.nvim'
