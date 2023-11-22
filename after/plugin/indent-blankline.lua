972e3a579302fa6903c42001343cc4d385cf2c98
john <johngemmell124@gmail.com>
2023-11-21 19:04:09 -0500

    config for indent blankline

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -2,6 +2,32 @@
 vim.opt.list = false
 
 require("ibl").setup {
-	show_current_context = true,
-	show_current_context_start = false,
+	scope = {
+		enabled = true;
+		show_start = false;
+		show_end = false;
+		--[[ highlight = { "Function", "Label" }, ]]
+		show_exact_scope = false;
+		include = {
+			--[[ node_type = { lua = { "return_statement", "table_constructor" } }, ]]
+			node_type = { ["*"] = { "*" } },
+		};
+	};
+
+
+	indent = {
+		char = '▏', -- right
+		--[[ char = '▕' -- left ]]
+        --[[ char = '│', -- center ]]
+		smart_indent_cap = true
+	};
+	whitespace = { highlight = { "Whitespace", "NonText" } },
 }
+
+
+local function one ()
+	local me = 1;
+	while true do
+		print('hello');
+	end
+end
