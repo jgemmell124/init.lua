009943b258f527ebcad90c32b91b44c57864abbe
john <johngemmell124@gmail.com>
2023-11-22 18:43:00 -0500

    gruvbox settings

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,6 +1,33 @@
 --let g:gruvbox_italic=1
 vim.g.gruvbox_italic=1
 
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = false,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+--[[ vim.cmd("colorscheme gruvbox") ]]
+
 -- Lua:
 -- For dark theme (neovim's default)
 vim.o.background = 'dark'
@@ -55,10 +82,11 @@ require('onedark').setup  {
     },
 }
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--[[ vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) ]]
+--[[ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) ]]
 
 -- Only Load one theme 
 --[[ require('onedark').load() ]]
 --[[ require('vscode').load() ]]
-vim.cmd("colorscheme darkplus")
+-- best vim theme
+--[[ vim.cmd("colorscheme darkplus") ]]
