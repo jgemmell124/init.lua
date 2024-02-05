f92656d99db3d8a9e805ea8568ccb3419e509615
john <johngemmell124@gmail.com>
2024-02-05 15:49:01 -0500

    change copilot compeltion key

--- lua/user/setup.lua
+++ lua/user/setup.lua
@@ -41,8 +41,9 @@ opt.eol=false
 opt.eof=false
 
 
-vim.keymap.set('i', '<C-CR>', 'copilot#Accept("\\<CR>")', {
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
 	expr = true,
 	replace_keycodes = false
 })
+
 vim.g.copilot_no_tab_map = true
